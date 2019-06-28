dced8fb981d6863d77dd69f905e9f02a1460b116
Peter Jang <peter.x.jang@gmail.com>
2019-06-27 20:27:57 -0500

    Add RESTful contact actions

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,11 +1,38 @@
 class Api::ContactsController < ApplicationController
-  def one_contact_method
-    @contact = Contact.first
-    render "one_contact.json.jb"
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
   end
 
-  def all_contacts_method
-    @contacts = Contact.all
-    render "all_contacts.json.jb"
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.last_name = params[:last_name] || @contact.last_name
+    @contact.email = params[:email] || @contact.email
+    @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render json: { message: "Contact successfully destroyed!" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/one_contact_url" => "contacts#one_contact_method"
-    get "/all_contacts_url" => "contacts#all_contacts_method"
+    get "/contacts" => "contacts#index"
+    post "/contacts" => "contacts#create"
+    get "/contacts/:id" => "contacts#show"
+    patch "/contacts/:id" => "contacts#update"
+    delete "/contacts/:id" => "contacts#destroy"
   end
 end
