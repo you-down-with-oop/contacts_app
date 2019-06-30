ead58b93d0af796dc34fe84a80dc7351c7367e6c
Peter Jang <peter.x.jang@gmail.com>
2019-06-30 11:40:02 -0500

    Add model methods to format data

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    "#{last_name}, #{first_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone_number}"
+  end
 end
