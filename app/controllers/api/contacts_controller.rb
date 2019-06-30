69e3c6c481747da4c02579fec81a403f41c11d31
Peter Jang <peter.x.jang@gmail.com>
2019-06-30 12:02:12 -0500

    Update controller methods to reflect new model attributes

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -7,9 +7,11 @@ class Api::ContactsController < ApplicationController
   def create
     @contact = Contact.new(
       first_name: params[:first_name],
+      middle_name: params[:middle_name],
       last_name: params[:last_name],
       email: params[:email],
       phone_number: params[:phone_number],
+      bio: params[:bio],
     )
     @contact.save
     render "show.json.jb"
@@ -23,9 +25,11 @@ class Api::ContactsController < ApplicationController
   def update
     @contact = Contact.find_by(id: params[:id])
     @contact.first_name = params[:first_name] || @contact.first_name
+    @contact.middle_name = params[:middle_name] || @contact.middle_name
     @contact.last_name = params[:last_name] || @contact.last_name
     @contact.email = params[:email] || @contact.email
     @contact.phone_number = params[:phone_number] || @contact.phone_number
+    @contact.bio = params[:bio] || @contact.bio
     @contact.save
     render "show.json.jb"
   end
