ae567450eaea6bd67ffeaa5c2e9b757dd4a93e3a
Peter Jang <peter.x.jang@gmail.com>
2019-07-08 18:44:22 -0500

    Change contacts index to only show contacts you own

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -1,6 +1,6 @@
 class Api::ContactsController < ApplicationController
   def index
-    @contacts = Contact.all
+    @contacts = current_user.contacts
 
     if params[:search]
       @contacts = @contacts.where("first_name ILIKE ? OR last_name ILIKE ? OR email ILIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%")
